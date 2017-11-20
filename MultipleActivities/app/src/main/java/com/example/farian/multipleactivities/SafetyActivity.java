122e8b095604c7b6b0c0e98baa1e60c0782493fa
Farhad Arian <arian@cip.ifi.lmu.de>
2017-11-20 23:40:28 +0100

    in SafteyActivity add logout to back in loginActivity

--- MultipleActivities/app/src/main/java/com/example/farian/multipleactivities/SafetyActivity.java
+++ MultipleActivities/app/src/main/java/com/example/farian/multipleactivities/SafetyActivity.java
@@ -1,7 +1,10 @@
 package com.example.farian.multipleactivities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * project activity
@@ -12,6 +15,14 @@ public class SafetyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safety);
+        Button logoutBtn=(Button)findViewById(R.id.saftey_logout_button);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
